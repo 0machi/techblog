124c07010267f6b934bf68c31f0bd8faf81b633f
Kohei Omachi <koomachi@yahoo-corp.jp>
2023-05-03 13:41:07 +0900

    rafactor: container blog

--- app/blogs/[blogId]/page.tsx
+++ app/blogs/[blogId]/page.tsx
@@ -1,9 +1,5 @@
-import { load } from 'cheerio'
-import hljs from 'highlight.js'
-import { notFound } from 'next/navigation'
-import 'highlight.js/styles/github-dark.css'
-import Blog from '../../components/presentational/blog'
-import { getBlogDetail, getBlogList } from '../../libs/microcms'
+import BlogContainer from '../../components/container/blogContainer'
+import { getBlogList } from '../../libs/microcms'
 
 export async function generateStaticParams() {
   const { contents } = await getBlogList()
@@ -17,30 +13,4 @@ export async function generateStaticParams() {
   return [...paths]
 }
 
-function highlightCodeBlock(html: string): string {
-  const $ = load(html)
-  $('pre code').each((_, elm) => {
-    const result = hljs.highlightAuto($(elm).text())
-    $(elm).html(result.value)
-    $(elm).addClass('hljs')
-  })
-  const highlightedHtml = $.html()
-  return highlightedHtml
-}
-
-export default async function StaticDetailPage({
-  params: { blogId },
-}: {
-  params: { blogId: string }
-}) {
-  const blog = await getBlogDetail(blogId)
-
-  if (!blog) {
-    notFound()
-  }
-
-  const blogHtml = blog.content
-  const highlightedBlogHtml = highlightCodeBlock(blogHtml)
-
-  return <Blog blogTitle={blog.title} blogHtml={highlightedBlogHtml} />
-}
+export default BlogContainer
