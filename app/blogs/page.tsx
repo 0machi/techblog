60a3ddb8bcb1edef82492f3c7c27ad7dfc07bd46
Kohei Omachi <koomachi@yahoo-corp.jp>
2023-05-03 11:49:18 +0900

    refactor: 不要な要素を削除

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -4,16 +4,12 @@ import { getBlogList } from '../libs/microcms'
 export default async function StaticPage() {
   const { contents } = await getBlogList()
 
-  // ページの生成された時間を取得
-  const time = new Date().toLocaleString()
-
   if (!contents || contents.length === 0) {
     return <h1>No contents</h1>
   }
 
   return (
     <div>
-      <h1>{time}</h1>
       <ul>
         {contents.map((blog) => {
           return (
