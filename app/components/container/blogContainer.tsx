3ee420ec0e3d51d142ea8d5a119780e7eaa01147
Kohei Omachi <koomachi@yahoo-corp.jp>
2023-05-06 14:28:47 +0900

    refactor: highlightCodeBlock

--- app/components/container/blogContainer.tsx
+++ app/components/container/blogContainer.tsx
@@ -1,20 +1,7 @@
-import { load } from 'cheerio'
-import hljs from 'highlight.js'
 import { notFound } from 'next/navigation'
-import 'highlight.js/styles/github-dark.css'
 import Blog from '../../components/presentational/blog'
+import { highlightCodeBlock } from '../../libs/highlightjs'
 import { getBlogDetail } from '../../libs/microcms'
-
-function highlightCodeBlock(html: string): string {
-  const $ = load(html)
-  $('pre code').each((_, elm) => {
-    const result = hljs.highlightAuto($(elm).text())
-    $(elm).html(result.value)
-    $(elm).addClass('hljs')
-  })
-  const highlightedHtml = $.html()
-  return highlightedHtml
-}
 
 export default async function BlogContainer({
   params: { blogId },
