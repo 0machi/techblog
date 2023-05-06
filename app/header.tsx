f106b75429a5d2b457133e7d1e60780bba4acdfa
shinaps <axon@shinaps.jp>
2023-05-07 00:22:20 +0900

    feat: Do not show link when it is empty

--- app/header.tsx
+++ app/header.tsx
@@ -3,12 +3,16 @@ import HeaderBorder from '@/components/presentational/headerBorder'
 import { robotoBold } from '@/styles/fonts'
 
 export default function Header() {
+  const linkToOurServices = ''
+
   return (
     <header>
       <div
         className={`h-16 max-w-5xl mx-auto flex justify-between items-center border-x border-dashed border-slate-200`}>
         <h1 className={`${robotoBold.className} text-2xl px-3`}>shinaps</h1>
-        <a href='#' className={`px-3`}>Our services <NavigateNextIcon fontSize={`small`} /></a>
+        {linkToOurServices && (
+          <a href={linkToOurServices} className={`px-3`}>Our services <NavigateNextIcon fontSize={`small`} /></a>
+        )}
       </div>
       <HeaderBorder />
     </header>
