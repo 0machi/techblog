8dc44704b2dd759dbe4676ea4a6f95f922a053c7
shinaps <axon@shinaps.jp>
2023-05-07 23:22:59 +0900

    add: add icon for twitter and github

--- config/contactList.ts
+++ config/contactList.ts
@@ -1,6 +1,5 @@
 import type { Service } from '@/types'
 
-export const contactList: Service[] = [
-  { name: 'Twitter', url: 'https://twitter.com/sh1n4ps' },
-  { name: 'GitHub', url: 'https://github.com/sh1n4ps' },
-]
+export const twitter = { name: 'Twitter', url: 'https://twitter.com/sh1n4ps' }
+export const github = { name: 'GitHub', url: 'https://github.com/sh1n4ps' }
+export const contactList: Service[] = [twitter, github]
