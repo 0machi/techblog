a05b95680c17325fa5b0c4650a010cfac129642a
shinaps <axon@shinaps.jp>
2023-05-07 16:07:35 +0900

    feat: added OBP image

--- app/header.tsx
+++ app/header.tsx
@@ -1,4 +1,3 @@
-import NavigateNextIcon from '@mui/icons-material/NavigateNext'
 import HeaderBorder from '@/components/presentational/headerBorder'
 import { robotoBold } from '@/styles/fonts'
 
@@ -11,7 +10,7 @@ export default function Header() {
         className={`h-16 max-w-5xl mx-auto flex justify-between items-center border-x border-dashed border-slate-200`}>
         <h1 className={`${robotoBold.className} text-2xl px-3`}>shinaps</h1>
         {linkToOurServices && (
-          <a href={linkToOurServices} className={`px-3`}>Our services <NavigateNextIcon fontSize={`small`} /></a>
+          <a href={linkToOurServices} className={`px-3`}>Our services</a>
         )}
       </div>
       <HeaderBorder />
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
-'use client'
 import '@/styles/globals.css'
-import React, { useEffect } from 'react'
+import { Metadata } from 'next'
 import Footer from '@/app/footer'
 import Header from '@/app/header'
 import HorizontalLine from '@/components/presentational/horizontalLine'
@@ -8,11 +7,42 @@ import { contactList } from '@/config/contactList'
 import { serviceList } from '@/config/serviceList'
 import { robotoRegular } from '@/styles/fonts'
 
+const siteName = 'shinaps tech blog'
+const description = 'shinaps　tech blog'
+const url = 'https://shinaps-techblog.vercel.app/'
+
+export const metadata: Metadata = {
+  title: {
+    default: siteName,
+    template: `%s - ${siteName}`,
+  },
+  description,
+  openGraph: {
+    title: siteName,
+    description,
+    url,
+    siteName,
+    locale: 'ja_JP',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: siteName,
+    description,
+    site: '@sh1n4ps',
+    creator: '@sh1n4ps',
+  },
+  // verification: {
+  //   google: 'サーチコンソールのやつ',
+  // },
+  alternates: {
+    canonical: url,
+  },
+}
+
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  useEffect(() => {
-    // @ts-ignore
-    import('preline')
-  }, [])
+
   return (
     <html lang='jp' className={`${robotoRegular.className} bg-white`}>
     <body>
