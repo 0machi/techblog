9e4539209bfa6cdd0944818df482de11d8b2772c
Kohei Omachi <koomachi@yahoo-corp.jp>
2023-05-10 20:30:15 +0900

    fix: customFetch

--- libs/microcms.ts
+++ libs/microcms.ts
@@ -7,6 +7,15 @@ import type { Article, Category } from '@/types'
 export const client = createClient({
   serviceDomain: microCMSConfig.serviceDomain,
   apiKey: microCMSConfig.apiKey,
+  customFetch: (input, init) => {
+    if (typeof input === 'string') {
+      const newInput = new URL(input)
+      const time = new Date()
+      newInput.searchParams.set('cacheclearparam', `${time.getMilliseconds()}`)
+      return fetch(newInput.href, init)
+    }
+    return fetch(input, init)
+  },
 })
 
 const articleEndpoint = 'articles'
