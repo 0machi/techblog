e066a969cabe0d6b7109183f0a3191a2ad7e1890
Kohei Omachi <koomachi@yahoo-corp.jp>
2023-05-14 22:27:51 +0900

    Revert "test: twitter_card"
    This reverts commit 2beccb73331e66f40acfd550bca224313d3e5089.

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export const metadata: Metadata = {
     type: 'website',
   },
   twitter: {
-    // card: 'summary_large_image',
+    card: 'summary_large_image',
     title: siteName,
     description,
     site: '@sh1n4ps',
