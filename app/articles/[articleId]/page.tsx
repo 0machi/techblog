431beffad10df7281e7650e8c8f71a896e68378f
shinaps <axon@shinaps.jp>
2023-09-20 21:17:30 +0900

    feat: Added dynamic retrieval of metadata title and description

--- app/articles/[articleId]/page.tsx
+++ app/articles/[articleId]/page.tsx
@@ -1,7 +1,40 @@
+import { Metadata, ResolvingMetadata } from 'next'
 import ArticleContainer from '@/components/container/article/articleContainer'
-import { fetchArticleList } from '@/libs/microcms/article'
+import { fetchArticle, fetchArticleList } from '@/libs/microcms/article'
 
 export const dynamicParams = false
+
+type Props = {
+  params: { articleId: string }
+}
+
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata,
+): Promise<Metadata> {
+  const article = await fetchArticle(params.articleId)
+  const title = article.title
+  const description = article.summary
+
+  return {
+    title: title,
+    description,
+    openGraph: {
+      title: title,
+      description,
+      siteName: 'shinaps tech blog',
+      locale: 'ja_JP',
+      type: 'website',
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: title,
+      description,
+      site: '@sh1n4ps',
+      creator: '@sh1n4ps',
+    },
+  }
+}
 
 export async function generateStaticParams() {
   const { contents } = await fetchArticleList()
