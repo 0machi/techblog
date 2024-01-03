664c20470293337a483553a5ddfc7abd71b1fd8f
Kohei Omachi <koomachi@yahoo-corp.jp>
2024-01-04 00:33:30 +0900

    fix: インラインコードがハイライトされていないため、ハイライト

--- components/container/article/articleDraftContainer.tsx
+++ components/container/article/articleDraftContainer.tsx
@@ -1,6 +1,6 @@
 import { notFound, redirect } from 'next/navigation'
 import ArticlePage from '@/components/presentational/article/articlePage'
-import { highlightCodeBlock } from '@/libs/highlightjs'
+import { highlightCode } from '@/libs/highlightjs'
 import { getToc } from '@/libs/markdown'
 import { fetchArticle } from '@/libs/microcms/article'
 
@@ -21,7 +21,7 @@ export default async function ArticleContainer({
   if (!article) notFound()
 
   const articleHtml = article.content
-  const highlightedArticleHtml = highlightCodeBlock(articleHtml)
+  const highlightedArticleHtml = highlightCode(articleHtml)
   const toc = getToc(articleHtml)
 
   return <ArticlePage toc={toc} article={article} html={highlightedArticleHtml} />
